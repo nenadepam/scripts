fb128e8bf709b928b4d4e0a10e065974c269f42a
nenadepam <156675093+nenadepam@users.noreply.github.com>
2024-10-17 16:01:00 +0200

    Update logserver.js

--- logserver.js
+++ logserver.js
@@ -7,10 +7,15 @@ const port = process.env.PORT || 8080;
 
 // Define a simple route
 app.get('/', async  (req, res) => {
- await fs.appendFile('logs.txt', 'tests');
   res.send('Hello, this is your Node.js server running locally!');
 });
 
+app.post('/', (req, res) => {
+    console.log("Alen send:")
+    console.log(JSON.stringify(req.body))
+    res.sendStatus(200)
+  });
+  
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
